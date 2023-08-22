a4c9617a8abdc013b05ca1b31748f46c8b00748f
ancer7 <ancerafriono@gmail.com>
2023-08-22 09:48:55 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,7 @@ module "s3-bucket" {
   version = "3.14.1"
 }
 
-credentials "app.terraform.io" {
-  # valid user API token
-  token = "KhTXYQF6ewqXCg.atlasv1.OOpCZYYlvjTRhv0ERyPHBtj9m4k3FLv3xamiTizTUw2avCf8FdpgPTJ2OEp2XQYuvPo"
-}
+# credentials "app.terraform.io" {
+#   # valid user API token
+#   token = "KhTXYQF6ewqXCg.atlasv1.OOpCZYYlvjTRhv0ERyPHBtj9m4k3FLv3xamiTizTUw2avCf8FdpgPTJ2OEp2XQYuvPo"
+# }
