97484573a289a55a3e7e9a9c60725cb76c1070a8
ancer7 <ancerafriono@gmail.com>
2023-08-22 09:42:15 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,9 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/afristore/s3-bucket/aws"
-  version = "1.0.0"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.14.1"
+}
+
+credentials "app.terraform.io" {
+  # valid user API token
+  token = "KhTXYQF6ewqXCg.atlasv1.OOpCZYYlvjTRhv0ERyPHBtj9m4k3FLv3xamiTizTUw2avCf8FdpgPTJ2OEp2XQYuvPo"
 }
